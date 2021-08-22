c65a0e2a07b45ead3cb83f1aa42c723e1b62c7d1
jychoi0918 <jychoi091808@gmail.com>
2021-08-22 17:49:50 +0900

    Validation Check Post User
    validation추가 후  CustomizeResponseEntityExceptionHandler가 상속 한 ResponseEntityExceptionHandler 의 handleMethodArgumentNotValid를 재정의(override) 하여 ExceptionResponse 생성 후 반환
    
    그러면 원하는 에러메시지를 알 수 있어 식별하기 쉽다. .

--- RESTful API/src/main/java/com/example/restful_web_service/exception/CustomizeResponseEntityExceptionHandler.java
+++ RESTful API/src/main/java/com/example/restful_web_service/exception/CustomizeResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.restful_web_service.exception;
 
 import com.example.restful_web_service.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,16 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
                 new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
 
         return  new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    //ResponseEntityExceptionHandler 의 안에있는 메소드 재정의
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        ExceptionResponse exceptionResponse =   //ex.getMessage()를 ↓ 변경 간결하게 사용하기 위해
+                new ExceptionResponse(new Date(),"Validation Failed",ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- RESTful API/src/main/java/com/example/restful_web_service/user/User.java
+++ RESTful API/src/main/java/com/example/restful_web_service/user/User.java
@@ -14,7 +14,7 @@ public class User {
 
     private Integer id;
 
-    @Size(min=2)
+    @Size(min=2, message = "Name은 2글자 이상 입력해 주세요")
     private String name;
 
     @Past
