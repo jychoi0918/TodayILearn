fc432a45779717ba8cc22a7abf923e0a050d4e60
jychoi0918 <jychoi091808@gmail.com>
2021-08-26 23:59:34 +0900

    Internationalization message
    - yml 파일 내 message 추가
    - messages.properties 언어별 추가
    -Application @Bean  locale 추가
    - controller에 messageSource @Autowired

--- RESTful API/src/main/java/com/example/restful_web_service/helloworld/HelloWorldController.java
+++ RESTful API/src/main/java/com/example/restful_web_service/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.example.restful_web_service.helloworld;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
     //Get
     // /hello-world -> (endPoint)
 
@@ -28,7 +36,13 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s ",name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized( @RequestHeader(name="Accept-Language",required = false)
+    Locale locale){
 
+
+        return messageSource.getMessage("greeting.message",null, locale);
+    }
 
 
 }
