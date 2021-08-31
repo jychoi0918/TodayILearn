7c5d902778e39d153e1424c070941f4d0d3b83e2
jychoi0918 <jychoi091808@gmail.com>
2021-08-31 14:06:30 +0900

    URI를 이용한 REST API Version 관리
    url을 변경하여 version 관리를 수행
    - UserV2 객체 생성 (User 상속)
    -controller에서 User->UserV2로 변경(BeansUtils.copyProperties)

--- RESTful API/src/main/java/com/example/restful_web_service/user/AdminUserController.java
+++ RESTful API/src/main/java/com/example/restful_web_service/user/AdminUserController.java
@@ -4,6 +4,8 @@ package com.example.restful_web_service.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,9 +47,40 @@ public class AdminUserController {
     }
 
 
+    /*버전관리*/
     //url 에 있는 값은 String이지만 매개변수 타입을 정하면 변한다.
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id){
+    @GetMapping("/v1/users/{id}")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+
+        //User -> User2
+        UserV2 userV2 = new UserV2();
+
+        //두 유틸 안에 공통적인 필드(properties) 있으면 해당값 복사
+        BeanUtils.copyProperties(user,userV2);
+        userV2.setGrade("VIP");
+
+
+
+        //가져올 값에 대한 필터 생성
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id","name","joinDate","grade","ssn");
+
+        //필터 추가
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo2",filter);
+
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
@@ -66,7 +99,6 @@ public class AdminUserController {
 
         return mapping;
     }
-
 
 
 
