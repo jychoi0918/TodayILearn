2bfde06c9f84648812d62efbae7d4d4919b8cb22
jychoi0918 <jychoi091808@gmail.com>
2021-09-11 23:49:43 +0900

    JPQL 맛보기 - JPQL은 객체지향 SQL
    테이블이 아닌 객체를 대상화한 SQL

--- JPA 프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA 프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,13 +18,17 @@ public class JpaMain {
 
         try {
             //logic
-            //조회
+            //수정
             Member findMember = em.find(Member.class, 1L);
-            System.out.println("member = " + findMember.getId());
+            //JPQL을 이용하여 페이징을 쉽게 할 수 있다.
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
 
-
-            //삭제
-            em.remove(findMember);
+            for(Member member:resultList){
+                System.out.println("'member' = " + member.getName());
+            }
 
             tx.commit();
         }catch (Exception e){
