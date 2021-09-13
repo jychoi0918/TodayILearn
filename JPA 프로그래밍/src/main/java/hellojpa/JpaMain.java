6162a0fce30659d802b5427800d445fad8fa7d06
jychoi0918 <jychoi091808@gmail.com>
2021-09-13 23:45:02 +0900

    영속성 컨텍스트- 비영속성과 영속성
    영속성 컨텍스트
    -JPA를 이해하는데 가장 중요한 용어
    -"엔티티를 영구 저장하는 환경" 이라는 뜻
    -EntityManager.persist(entity);
    persist 메소드는 DB에 저장하는 것이 아니라 Entity를 영속성 컨텍스트라는 곳에 저장하는 것
    
    영속성 컨텍스트는 논리적인 개념, 눈에 보이지 않는다. 엔티티 매니저를 통해서 영속성 컨텍스트에 접근

--- JPA 프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA 프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -17,17 +17,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //logic
-            Member findMember = em.find(Member.class, 1L);
-            //JPQL을 이용하여 페이징을 쉽게 할 수 있다.
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("Name");
 
-            for(Member member:resultList){
-                System.out.println("'member' = " + member.getName());
-            }
+//            >>>>>>>>>>>>>여기까지 비영속성
+
+//            여기부터 영속성<<<<<<<<<<<<<<<<<<<
+           em.persist(member);
+           //em.detatch(member); 는 영속성 컨텍스트 분리
+            //em.remove(member); 는 객체를 삭제한 상태
 
             tx.commit();
         }catch (Exception e){
