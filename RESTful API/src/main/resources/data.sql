c7f508890c1c7d22d4a7eea2bb56a9c800e31989
jychoi0918 <jychoi091808@gmail.com>
2021-10-11 23:01:19 +0900

    JPA 초기 데이터 생성 sql 생성 및 yml 파일 h2 설정

---
+++ RESTful API/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'User1','test1111','950918-2000000');
+insert into user values(2, sysdate(), 'User2','test2222','110728-2000000');
+insert into user values(3, sysdate(), 'User3','test3333','141735-2000000');
