3a6ae385fc941ec557720a74687f37b88f92fa2d
jychoi0918 <jychoi091808@gmail.com>
2021-10-16 22:35:14 +0900

    JPA를 이용한 사용자 목록 조회 - HATEOUS 버전 차이로 더 찾아보기.

--- RESTful API/src/main/java/com/example/restful_web_service/user/UserJpaController.java
+++ RESTful API/src/main/java/com/example/restful_web_service/user/UserJpaController.java
@@ -1,11 +1,15 @@
 package com.example.restful_web_service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -17,6 +21,22 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        //User user = userRepository.findById(id);
+        Optional<User> user = userRepository.findById(id);
+
+        if(!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID{%s} not found",id));
+        }
+
+        //HATEOUS 기능
+        EntityModel<User> resource = new EntityModel<>(user.get());
+//        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllusers());
+//       resource.add(linkTo.withRel("all-users"));
+        return resource;
     }
 
 }
